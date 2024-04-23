c24e1aabb2c6d6407d2e1ae5c8348d2112948cdc
Ramyrashad127 <ramy41599@gmail.com>
2024-04-23 12:59:04 +0200

    new

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+"""import modules"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    tasks = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id))
+    name = tasks.json().get("name")
+    data =  requests.get("https://jsonplaceholder.typicode.com/todos/{}".format(id))
+    ans = data.json()
+    done_tasks = []
+    cnt_done = 0
+    cnt_tot = 0
+    for ele in ans:
+        if ele.get('userId') == int(id):
+            cnt_tot += 1
+            if ele.get("completed"):
+                 cnt_done += 1
+                 done_tasks.append(ele.get("title"))
+    print("Employee {} is done with tasks({}}/{}}):".format(name, cnt_done, cnt_tot))
+    for task in done_tasks:
+        print("\t {}".format(task))
+
