2103112463763b1b2bfd7eac580ca8106c5a65a3
Ramyrashad127 <ramy41599@gmail.com>
2024-04-23 13:22:19 +0200

    new

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         "https://jsonplaceholder.typicode.com/users/{}".format(id))
     name = tasks.json().get("name")
     data = requests.get(
-        "https://jsonplaceholder.typicode.com/todos/{}".format(id))
+        "https://jsonplaceholder.typicode.com/todos/")
     ans = data.json()
     done_tasks = []
     cnt_done = 0
@@ -23,28 +23,7 @@ if __name__ == "__main__":
             if ele.get("completed"):
                 cnt_done += 1
                 done_tasks.append(ele.get("title"))
-    print("Employee {} is done with tasks({}/{}}):"
+    print("Employee {} is done with tasks({}/{}):"
           .format(name, cnt_done, cnt_tot))
     for task in done_tasks:
         print("\t{}".format(task))
-if __name__ == "__main__":
-    id = sys.argv[1]
-    tasks = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(id))
-    name = tasks.json().get("name")
-    data = requests.get(
-        "https://jsonplaceholder.typicode.com/todos/{}".format(id))
-    ans = data.json()
-    done_tasks = []
-    cnt_done = 0
-    cnt_tot = 0
-    for ele in ans:
-        if ele.get('userId') == int(id):
-            cnt_tot += 1
-            if ele.get("completed"):
-                cnt_done += 1
-                done_tasks.append(ele.get("title"))
-    print("Employee {} is done with tasks({}}/{}}):"
-          .format(name, cnt_done, cnt_tot))
-    for task in done_tasks:
-        print("\t {}".format(task))
