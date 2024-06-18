8210a07b40d5fc2f0360910cc6c139c00efe6b01
Ramyrashad127 <ramy41599@gmail.com>
2024-06-18 16:03:43 -0700

    new

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """new function """
-    headers = {'User-Agent': "My-User-Agent"}
+    headers = {'User-Agent': "user-Agent"}
     response = requests.get(
             'https://www.reddit.com/r/{}/about.json'.format(subreddit),
             headers=headers, allow_redirects=False)
 
-    if response.status_code >= 200:
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    else:
         return 0
-
-    return response.json().get('data').get('subscribers')
