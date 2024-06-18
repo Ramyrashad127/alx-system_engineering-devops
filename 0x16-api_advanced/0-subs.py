114d0d17d6f4c275e7d6c4923cf80911089429cc
Ramyrashad127 <ramy41599@gmail.com>
2024-06-18 15:22:51 -0700

    new

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
+""" import modules """
 import requests
 
 def number_of_subscribers(subreddit):
+    """new function """
     headers = {'User-Agent': 'my-app/0.0.1'}
     response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers, allow_redirects    =False)
 
